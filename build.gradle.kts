78e999a6a50d9a8753de6f2e50bd85e1d3f5f491
xyber1004 <samratmukherjee.sps@gmail.com>
2025-06-23 08:12:53 +0530

    Checkpoint: created bottom navigation bar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,4 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-
-    // system UI Controller
-    impplementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
-
-    // Extended Icons
-    impplementation("androidx.compose.material:material-icons-extended:1.5.4")
 }
